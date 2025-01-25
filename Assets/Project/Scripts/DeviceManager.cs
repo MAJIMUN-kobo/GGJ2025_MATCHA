451ac1dfd4d784c45e36c8fd1efd787e2d47495d
Kosuke Kaneko <pumpkinkaneko@gmail.com>
2025-01-25 14:25:40 +0900

    Add: UIを管理できるようにUI関係のスクリプトを追加しました。 Text Mesh Proを使ってますが、追加Assetが別環境で動くか不明だったので、とりあえず、gitignoreで外しているので、ローカルで追加をお願いします。

---
+++ Assets/Project/Scripts/DeviceManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceManager : MonoBehaviour
+{
+    private Vector2 normalizedMousePosition = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        this.normalizedMousePosition = new Vector2(mousePosition.x / screenWidth, mousePosition.y / screenHeight);
+    }
+
+    public Vector2 getMousePositionNormalized() {
+        return this.normalizedMousePosition;
+    }
+}
--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.iOS;
 
 public class GameManager : MonoBehaviour
 {
+    public Canvas uIManager;
+    private DeviceManager deviceManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.deviceManager = this.GetComponent<DeviceManager>();            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = this.deviceManager.getMousePositionNormalized();
+        this.uIManager.GetComponent<UIMousePosition>().showPosition(position);
     }
 }
